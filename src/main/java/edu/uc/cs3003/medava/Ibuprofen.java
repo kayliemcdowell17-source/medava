9f7b64b76bd303c20d0554d9ecb84be070d141f9
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 23:42:12 -0500

    Update Ibuprofen.java

--- src/main/java/edu/uc/cs3003/medava/Ibuprofen.java
+++ src/main/java/edu/uc/cs3003/medava/Ibuprofen.java
@@ -1,7 +1,6 @@
 package edu.uc.cs3003.medava;
 
 public class Ibuprofen extends Medicine {
-
     public Ibuprofen() {
         super("Ibuprofen");
     }
@@ -10,14 +9,5 @@ public class Ibuprofen extends Medicine {
     public MedicineSchedule getSchedule() {
         return MedicineSchedule.Uncontrolled;
     }
-
-    @Override
-    public double minimumTemperature() {
-        return 30.0;
-    }
-
-    @Override
-    public double maximumTemperature() {
-        return 90.0;
-    }
 }
+
