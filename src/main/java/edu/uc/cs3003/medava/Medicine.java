ea59c15f9622e62f16ba0607de26d181cd845d36
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 22:11:10 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,21 @@
 package edu.uc.cs3003.medava;
 
-public class Medicine {
+public abstract class Medicine implements Shippable {
+    private String mName;
 
+    public Medicine(String name) {
+        mName = name;
+    }
+
+    public String getMedicineName() {
+        return mName;
+    }
+
+    public abstract MedicineSchedule getSchedule();
+    public abstract double minimumTemperature();
+    public abstract double maximumTemperature();
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        return (minimumTemperature() <= lowTemperature && highTemperature <= maximumTemperature());
+    }
 }
