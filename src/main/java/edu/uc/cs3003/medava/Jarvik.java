b34e837c26bf5b4fa8a88ae8351c8cea057afa72
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 23:45:00 -0500

    Update Jarvik.java

--- src/main/java/edu/uc/cs3003/medava/Jarvik.java
+++ src/main/java/edu/uc/cs3003/medava/Jarvik.java
@@ -1,29 +1,13 @@
 package edu.uc.cs3003.medava;
 
-public class Jarvik implements Shippable {
-    private String serialNumber;
-
-    public Jarvik(String deviceSerialNumber) {
-        serialNumber = deviceSerialNumber;
-    }
-
-    @Override
-    public String getMedicineName() {
-        return "Jarvik Artificial Heart";
+public class Jarvik extends Medicine {
+    public Jarvik() {
+        super("Jarvik Artificial Heart");
     }
 
     @Override
     public MedicineSchedule getSchedule() {
         return MedicineSchedule.Uncontrolled;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    @Override
-    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
-        return (30.0 <= lowTemperature && highTemperature <= 90.0);
     }
 }
 
