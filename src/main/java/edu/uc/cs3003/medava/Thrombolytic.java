2fd9451dfe0582bbbfece11db9adf3684c3b7526
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 22:13:20 -0500

    Update Thrombolytic.java

--- src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
+++ src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
@@ -2,4 +2,25 @@ package edu.uc.cs3003.medava;
 
 public class Thrombolytic extends Medicine {
 
+    public Thrombolytic() {
+        super("Thrombolytic");
+    }
+
+    @Override
+    public MedicineSchedule getSchedule() {
+        return MedicineSchedule.Uncontrolled;
+    }
+
+    @Override
+    public double minimumTemperature() {
+        System.out.println("Getting the minimum safe temperature for a Thrombolytic drug.");
+        return 36.0;
+    }
+
+    @Override
+    public double maximumTemperature() {
+        System.out.println("Getting the maximum safe temperature for a Thrombolytic drug.");
+        return 38.0;
+    }
 }
+
