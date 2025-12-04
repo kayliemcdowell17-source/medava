88e6004de03885a3227fdc750ac440dee9e094d9
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 22:13:50 -0500

    Update Oxycodone.java

--- src/main/java/edu/uc/cs3003/medava/Oxycodone.java
+++ src/main/java/edu/uc/cs3003/medava/Oxycodone.java
@@ -2,4 +2,22 @@ package edu.uc.cs3003.medava;
 
 public class Oxycodone extends Medicine {
 
+    public Oxycodone() {
+        super("Oxycodone");
+    }
+
+    @Override
+    public MedicineSchedule getSchedule() {
+        return MedicineSchedule.Two;
+    }
+
+    @Override
+    public double minimumTemperature() {
+        return 15.0;
+    }
+
+    @Override
+    public double maximumTemperature() {
+        return 25.0;
+    }
 }
