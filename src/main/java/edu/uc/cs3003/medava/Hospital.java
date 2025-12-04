261e58a734fea27bdc67d51177f039c9e6170d7e
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 22:18:54 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,5 +1,23 @@
 package edu.uc.cs3003.medava;
 
 public class Hospital {
+    private String name;
 
+    public Hospital(String hospitalName) {
+        name = hospitalName;
+    }
+
+    void receive(Transporter t) {
+        while (!t.isEmpty()) {
+            Shippable unloaded = t.unload();
+            System.out.println(String.format("Checking whether Hospital can receive %s.", unloaded.getMedicineName()));
+            if (unloaded.getSchedule() != MedicineSchedule.Uncontrolled) {
+                System.out.println(String.format("Hospital cannot receive controlled substances and %s is a %s.",
+                        unloaded.getMedicineName(), unloaded.getSchedule().asString()));
+            } else {
+                System.out.println(String.format("Accepted a shipment of %s.", unloaded.getMedicineName()));
+            }
+        }
+    }
 }
+
