d1b0014388443389f087df273e1d59bebba49a46
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 23:41:33 -0500

    Update MedicineSchedule.java

--- src/main/java/edu/uc/cs3003/medava/MedicineSchedule.java
+++ src/main/java/edu/uc/cs3003/medava/MedicineSchedule.java
@@ -1,27 +1,16 @@
 package edu.uc.cs3003.medava;
 
 public enum MedicineSchedule {
-    One(1),
-    Two(2),
-    Three(3),
-    Four(4),
-    Five(5),
-    Uncontrolled(6);
+    Uncontrolled("Uncontrolled"),
+    ScheduleTwo("Schedule Two");
 
-    private int mSchedule;
+    private final String scheduleName;
 
-    private MedicineSchedule(int schedule) {
-        mSchedule = schedule;
+    MedicineSchedule(String name) {
+        scheduleName = name;
     }
 
     public String asString() {
-        switch (mSchedule) {
-            case 1: return "Schedule One Medicine";
-            case 2: return "Schedule Two Medicine";
-            case 3: return "Schedule Three Medicine";
-            case 4: return "Schedule Four Medicine";
-            case 5: return "Schedule Five Medicine";
-            default: return "Uncontrolled";
-        }
+        return scheduleName;
     }
 }
