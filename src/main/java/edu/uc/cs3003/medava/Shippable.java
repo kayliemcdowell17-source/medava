6e311bdd8ccc8bb3e0e1d085d9400eb7c26a14d5
kayliemcdowell17-source <kayliemcdowell17@gmail.com>
2025-12-03 22:08:22 -0500

    Update Shippable.java

--- src/main/java/edu/uc/cs3003/medava/Shippable.java
+++ src/main/java/edu/uc/cs3003/medava/Shippable.java
@@ -1,5 +1,8 @@
 package edu.uc.cs3003.medava;
 
 public interface Shippable {
-
+    public MedicineSchedule getSchedule();
+    public String getMedicineName();
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature);
 }
+
